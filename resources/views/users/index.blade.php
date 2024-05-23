d0ddbcd0eb504d57789770a75ce36779a7b682d6
Azwel9nj <mulumbesimwinga@gmail.com>
2024-05-23 18:32:20 +0200

    users index updated

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,73 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Users
-        </h2>
-    </x-slot>
+    <main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Users</h1>
+        <nav>
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Users</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="justify-between d-flex justify-content-between">
+                            <h5 class="card-title">Users</h5>
+                            
+                        </div>
+                        <!-- Table with hoverable rows -->
+                        <table id="myTable" class="table table-hover">
+                        {{-- <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for courses.." class="shadow appearance-none border rounded w-1/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"> --}}
+                        <thead>
+                            <tr>
+                            
+                            {{-- <th scope="col">#</th> --}}
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Creation date</th>
+                            <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($users as $user)
+                            
+                            <tr>
+                                {{-- <th scope="row">1</th> --}}
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td>{{$user->created_at}}</td>
+                                <td>
+                                    <div class="btn-group flex" role="group" aria-label="Button group">
+                                        <form method="POST" action="{{ route('users.resetUserPassword', $user->id) }}" class="inline-block">
+                                            @csrf
+                                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l-none">
+                                                Reset
+                                            </button>
+                                        </form>
+                                        <a href="{{ route('users.edit', $user->id) }}">
+                                            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-l-none">
+                                                Edit
+                                            </button>
+                                        </a>
+                                    </div>
+                                </td> 
+                            </tr>                            
+                            @endforeach
+                        </tbody>
+                        </table>
+                        <!-- End Table with hoverable rows -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main><!-- End #main -->
+    
 
     <div class="py-12">
         
