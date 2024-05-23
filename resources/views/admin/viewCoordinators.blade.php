8844bc999da15502632ad84341a7bbc13ff264cd
Azwel9nj <mulumbesimwinga@gmail.com>
2024-05-23 18:27:21 +0200

    All Coordinators UI updated

--- resources/views/admin/viewCoordinators.blade.php
+++ resources/views/admin/viewCoordinators.blade.php
@@ -1,51 +1,71 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Coordinators') }}
-        </h2>
-    </x-slot>
+<main id="main" class="main">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 overflow-x-auto">
-                    <table class="table-auto w-full mt-4">
-                        <thead>
+    <div class="pagetitle">
+        <h1>Coordinators</h1>
+        <nav>
+            <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Coordinators</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+    <section class="section">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="justify-between d-flex justify-content-between">
+                            <h5 class="card-title">Coordinators</h5>
                             <form method="post" action="{{ route('admin.importCoordinators')}}">
                                 @csrf
-                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-r-none">
+                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-r-none p-4">
                                     Import
                                 </button>
                             </form>
+                        </div>
+                        <!-- Table with hoverable rows -->
+                        <table id="myTable" class="table table-hover">
+                        {{-- <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for courses.." class="shadow appearance-none border rounded w-1/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"> --}}
+                        <thead>
                             <tr>
-                                <th class="px-4 py-2">Firstname</th>
-                                <th class="px-4 py-2">Lastname</th>
-                                <th class="px-4 py-2">Programme Coordinated</th>
-                                {{-- <th class="px-4 py-2">Number Of Courses</th> --}}
-                                <th class="px-4 py-2">Actions</th>
+                            
+                            {{-- <th scope="col">#</th> --}}
+                            <th scope="col">Firstname</th>
+                            <th scope="col">Lastname</th>
+                            <th scope="col">Programme Coordinated</th>
+                            <th scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($results as $result)
-                            <tr class="border-t border-b hover:bg-gray-100">
-                                <td class="px-4 py-2">{{$result->Firstname}}</td>
-                                <td class="px-4 py-2">{{$result->Surname}}</td>
-                                <td class="px-4 py-2">{{$result->Name}}</td>
-                                {{-- <td class="px-4 py-2">{{$counts[$result->ID]}}</td> --}}
-                                <td class="px-4 py-2">
+                        @foreach($results as $result)
+                            @include('coordinator.components.uploadAssessmentTypeModal')
+                            @include('coordinator.components.viewAssessmentTypeModal')
+                            <tr>
+                                {{-- <th scope="row">1</th> --}}
+                                <td>{{$result->Firstname}}</td>
+                                <td>{{$result->Surname}}</td>
+                                <td>{{$result->Name}}</td>
+                                <td>
                                     <form method="GET" action="{{ route('admin.viewCoordinatorsCourses', ['basicInformationId' => encrypt($result->ID)]) }}">
                                         <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                             View
                                         </button>
                                     </form>
-                                </td>
-                            </tr>
+                                </td> 
+                            </tr>                            
                             @endforeach
-                            <!-- Add more rows as needed -->
                         </tbody>
-                    </table>
+                        </table>
+                        <!-- End Table with hoverable rows -->
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+</main><!-- End #main -->
+    
+
+    
 </x-app-layout>
