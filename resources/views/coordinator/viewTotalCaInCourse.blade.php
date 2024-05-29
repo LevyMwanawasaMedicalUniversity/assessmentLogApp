ed0b8541f629ce2589ffcaa03df8a111dbdeb158
Azwel9nj <mulumbesimwinga@gmail.com>
2024-05-29 16:45:46 +0200

    ADDED DUMMY BUTTON

--- resources/views/coordinator/viewTotalCaInCourse.blade.php
+++ resources/views/coordinator/viewTotalCaInCourse.blade.php
@@ -16,6 +16,7 @@
                         <div class="d-flex justify-content-between align-items-center mb-4">
                             <h5 class="card-title">Total CAs out of 40</h5>
                             <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search by student number.." class="shadow appearance-none border rounded w-1/4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <a href="" class="btn btn-primary">PUBLISH CA</a>
                         </div>
                         <!-- Table with hoverable rows -->
                         <table id="myTable" class="table table-hover">
