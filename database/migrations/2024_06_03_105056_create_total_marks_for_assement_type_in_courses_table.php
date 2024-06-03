dab42f3cb5ba9820240f31927e080e7ac2a3b45c
Azwel9nj <mulumbesimwinga@gmail.com>
2024-06-03 13:27:11 +0200

    dfdf

---
+++ database/migrations/2024_06_03_105056_create_total_marks_for_assement_type_in_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('total_marks_for_assement_type_in_courses', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('course_id')->unsigned();
+            $table->bigInteger('assessment_type_id')->unsigned();
+            $table->foreign('user_id', 'fk_user_id')->references('id')->on('users')->onDelete('cascade');            
+            $table->foreign('assessment_type_id', 'fk_assessment_type_id')->references('id')->on('assessment_types')->onDelete('cascade');
+            $table->float('total_marks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('total_marks_for_assement_type_in_courses');
+    }
+};
