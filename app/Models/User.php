99b8c9e438b165ef7ac678a8d522680e3fdc26ed
Azwel9nj <mulumbesimwinga@gmail.com>
2024-06-22 12:35:39 +0200

    Make it public

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use OwenIt\Auditing\Contracts\Auditable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements Auditable
+class User extends Authenticatable 
 {
-    use HasFactory, Notifiable, HasRoles, Auditable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
