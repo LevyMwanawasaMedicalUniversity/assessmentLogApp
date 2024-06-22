fcfe43607fc27a20709fb3ba57c60d58e9454735
Azwel9nj <mulumbesimwinga@gmail.com>
2024-06-22 13:52:12 +0200

    G

--- resources/views/emails/userCredentials.blade.php
+++ resources/views/emails/userCredentials.blade.php
@@ -8,7 +8,7 @@
     <p>{{ $details['body'] }}</p>
     <p><strong>Email:</strong> {{ $details['email'] }}</p>
     <p><strong>Password:</strong> {{ $details['password'] }}</p>
-    <p>Please click <a href="http://192.168.20.38/">here</a> to access the Continuous Assessment System and log in using your credentials.</p>
+    <p>Please click <a href="http://assessments.lmmu.ac.zm/">here</a> to access the Continuous Assessment System and log in using your credentials.</p>
     <p>Kindly note that the assessments system is only accessible within the Levy Mwanawasa Medical University network.</p>
     <p>Should you have any questions or require assistance, please do not hesitate to contact ICT.</p>
     <p>Best regards,</p>
