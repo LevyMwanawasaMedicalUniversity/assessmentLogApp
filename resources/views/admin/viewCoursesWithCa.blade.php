afb83156a24fc20b630c49e619df048468b0f2be
Azwel9nj <mulumbesimwinga@gmail.com>
2024-07-16 15:41:58 +0200

    table filter added

--- resources/views/admin/viewCoursesWithCa.blade.php
+++ resources/views/admin/viewCoursesWithCa.blade.php
@@ -49,5 +49,24 @@
         </div>
     </section>
 </main><!-- End #main -->
-
+<script>
+    function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0]; // Change the index based on the column you want to filter
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }       
+        }
+    }
+</script>
 </x-app-layout>
