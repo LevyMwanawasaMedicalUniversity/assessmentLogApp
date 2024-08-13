723d193622abce6db757f04c19b237cb8ce90f4a
Azwel9nj <mulumbesimwinga@gmail.com>
2024-08-13 12:29:10 +0200

    update axios vunerability

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
 window.axios = axios;
 
+axios.defaults.baseURL = process.env.MIX_APP_URL;
+
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
