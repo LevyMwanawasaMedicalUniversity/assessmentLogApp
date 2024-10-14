1c9de4ab1d15a4e049dedc3f11422cd60b061105
Azwel9nj <mulumbesimwinga@gmail.com>
2024-10-14 16:33:45 +0200

    another

--- resources/views/allStudents/continousAssessment/viewCa.blade.php
+++ resources/views/allStudents/continousAssessment/viewCa.blade.php
@@ -105,22 +105,23 @@
                                                             @else
                                                                 <tr>
                                                                     <td>{{$courseName}}-{{$courseCode}}</td>
-                                                                     @php
-                                                                        $numberOfUniqueInstances = App\Models\StudentsContinousAssessment::where('students_continous_assessments.course_id', $result->course_id)
-                                                                            ->where('students_continous_assessments.delivery_mode', $result->delivery_mode)
-                                                                            ->where('students_continous_assessments.study_id', $result->study_id)
-                                                                            ->whereNotNull('students_continous_assessments.component_id')
-                                                                            ->distinct('students_continous_assessments.component_id')
-                                                                            ->count('students_continous_assessments.component_id');
-                                                                    @endphp
-                                                                    <td>
-                                                                        @if ($numberOfUniqueInstances > 0)
-                                                                            <span class="badge bg-primary">{{ number_format($result->total_marks / $numberOfUniqueInstances, 2) }}</span> <b>/</b>
-                                                                            <span class="badge bg-secondary">40</span>
-                                                                        @else
-                                                                            <span class="badge bg-danger">No components available</span>
-                                                                        @endif
-                                                                    </td>
+                                                                        @php
+                                                                            $numberOfUniqueInstances = App\Models\StudentsContinousAssessment::where('students_continous_assessments.course_id', $result->course_id)
+                                                                                ->where('students_continous_assessments.delivery_mode', $result->delivery_mode)
+                                                                                ->where('students_continous_assessments.study_id', $result->study_id)
+                                                                                ->whereNotNull('students_continous_assessments.component_id')
+                                                                                ->distinct('students_continous_assessments.component_id')
+                                                                                ->count('students_continous_assessments.component_id');
+                                                                        @endphp
+                                                                        <td>
+                                                                            @if ($numberOfUniqueInstances > 0)
+                                                                                <span class="badge bg-primary">{{ number_format($result->total_marks / $numberOfUniqueInstances, 2) }}</span> <b>/</b>
+                                                                                <span class="badge bg-secondary">40</span>
+                                                                            @else
+                                                                                <span class="badge bg-danger">No components available</span>
+                                                                            @endif
+                                                                        </td>
+
                                                                     <td class="text-end">
                                                                         <form action="{{ route('docket.viewCaComponentsWithComponent', encrypt($result->course_id)) }}" method="GET">
                                                                             <input type="hidden" name="study_id" value="{{encrypt($result->study_id)}}">
