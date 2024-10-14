a42356b10c63663b7094057f57e036c78944b79f
Azwel9nj <mulumbesimwinga@gmail.com>
2024-10-14 16:27:05 +0200

    How

--- resources/views/allStudents/continousAssessment/viewCa.blade.php
+++ resources/views/allStudents/continousAssessment/viewCa.blade.php
@@ -105,7 +105,7 @@
                                                             @else
                                                                 <tr>
                                                                     <td>{{$courseName}}-{{$courseCode}}</td>
-                                                                    @php
+                                                                     @php
                                                                         $numberOfUniqueInstances = App\Models\StudentsContinousAssessment::where('students_continous_assessments.course_id', $result->course_id)
                                                                             ->where('students_continous_assessments.delivery_mode', $result->delivery_mode)
                                                                             ->where('students_continous_assessments.study_id', $result->study_id)
@@ -114,8 +114,12 @@
                                                                             ->count('students_continous_assessments.component_id');
                                                                     @endphp
                                                                     <td>
-                                                                        <span class="badge bg-primary">{{ number_format($result->total_marks / $numberOfUniqueInstances, 2) }}</span> <b>/</b>
-                                                                        <span class="badge bg-secondary">40</span>
+                                                                        @if ($numberOfUniqueInstances > 0)
+                                                                            <span class="badge bg-primary">{{ number_format($result->total_marks / $numberOfUniqueInstances, 2) }}</span> <b>/</b>
+                                                                            <span class="badge bg-secondary">40</span>
+                                                                        @else
+                                                                            <span class="badge bg-danger">No components available</span>
+                                                                        @endif
                                                                     </td>
                                                                     <td class="text-end">
                                                                         <form action="{{ route('docket.viewCaComponentsWithComponent', encrypt($result->course_id)) }}" method="GET">
